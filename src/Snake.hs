44fee8513c8050bd626261a29eed6fddb6ccacae
Brandon Chinn <brandonchinn178@gmail.com>
2021-10-02 17:15:19 -0700

    Refactor, show score

--- src/Snake.hs
+++ src/Snake.hs
@@ -24,19 +24,40 @@ gui window = do
   {-- DOM setup --}
 
   set' title "Snake" window
-  body <- getBody window
+  body <-
+    getBody window
+      & set style
+          [ ("display", "grid")
+          , ("align-items", "center")
+          , ("justify-content", "center")
+          , ("font-size", "16px")
+          ]
 
-  canvas <- UI.canvas
-  applyAll_ canvas
-    [ set' UI.height canvasHeight
-    , set' UI.width canvasWidth
-    , set' style [("border", "solid black 1px")]
+  scoreBox <-
+    UI.p
+      & sink UI.text (("Score: " <>) . show . getScore <$> stateBehavior)
+      & set style
+          [ ("text-align", "center")
+          , ("font-style", "bold")
+          ]
+
+  canvas <-
+    UI.canvas
+      & set UI.height canvasHeight
+      & set UI.width canvasWidth
+      & set style [("border", "solid black 1px")]
+
+  appendTo body
+    [ scoreBox
+    , canvas
     ]
-  appendTo body [canvas]
 
   {-- Event handling --}
 
-  timer <- UI.timer & sink UI.interval (getMillisPerFrame <$> stateBehavior)
+  timer <-
+    UI.timer
+      & sink UI.interval (getMillisPerFrame <$> stateBehavior)
+
   on UI.tick timer $ \_ -> do
     UI.clearCanvas canvas
     state <- liftIO . getNextState =<< currentValue stateBehavior
@@ -67,6 +88,40 @@ gui window = do
 
   UI.start timer
 
+drawFrame :: GameState -> UI.Canvas -> UI ()
+drawFrame state@GameState{target} canvas = do
+  -- draw snake
+  element canvas
+    & set UI.fillStyle snakeColor
+    & void
+  forM_ (snakeBody state) $ \snakePart -> do
+    let snakePartBox = coordinateToPixel snakePart
+    element canvas
+      & runAll
+          [ UI.beginPath
+          , UI.fillRect (pixelTopLeft snakePartBox) pixelWidth pixelHeight
+          , UI.closePath
+          , UI.fill
+          ]
+
+  -- draw target
+  let targetBox = coordinateToPixel target
+  element canvas
+    & set UI.fillStyle targetColor
+    & runAll
+        [ UI.beginPath
+        , fillCircle (pixelCenter targetBox) (pixelWidth / 2)
+        , UI.closePath
+        , UI.fill
+        ]
+    & void
+  where
+    snakeColor = UI.htmlColor "black"
+    targetColor = UI.htmlColor "red"
+    fillCircle center radius = UI.arc center radius 0 (2 * pi)
+
+{-- Game state --}
+
 getMillisPerFrame :: GameState -> Int
 getMillisPerFrame GameState{snakeTail} =
   let level = length snakeTail `div` targetsPerLevel
@@ -79,32 +134,8 @@ getMillisPerFrame GameState{snakeTail} =
     -- how many targets to consume before incrementing the level
     targetsPerLevel = 3
 
-drawFrame :: GameState -> UI.Canvas -> UI ()
-drawFrame state@GameState{target} canvas = do
-  -- draw snake
-  set' UI.fillStyle snakeColor canvas
-  forM_ (snakeBody state) $ \snakePart -> do
-    let snakePartBox = coordinateToPixel snakePart
-    applyAll_ canvas
-      [ UI.beginPath
-      , UI.fillRect (pixelTopLeft snakePartBox) pixelWidth pixelHeight
-      , UI.closePath
-      , UI.fill
-      ]
-
-  -- draw target
-  let targetBox = coordinateToPixel target
-  applyAll_ canvas
-    [ set' UI.fillStyle targetColor
-    , UI.beginPath
-    , fillCircle (pixelCenter targetBox) (pixelWidth / 2)
-    , UI.closePath
-    , UI.fill
-    ]
-  where
-    snakeColor = UI.htmlColor "black"
-    targetColor = UI.htmlColor "red"
-    fillCircle center radius = UI.arc center radius 0 (2 * pi)
+getScore :: GameState -> Int
+getScore = length . snakeTail
 
 {-- Keybindings --}
 
@@ -141,11 +172,11 @@ keyFromCode = \case
 
 {-- Utilities --}
 
-applyAll :: Monad m => a -> [a -> m b] -> m [b]
-applyAll a = mapM ($ a)
-
-applyAll_ :: Monad m => a -> [a -> m b] -> m ()
-applyAll_ a fs = void $ applyAll a fs
+runAll :: Monad m => [a -> m ()] -> m a -> m a
+runAll fs ma = do
+  a <- ma
+  mapM_ ($ a) fs
+  return a
 
 appendTo :: Element -> [Element] -> UI ()
 appendTo p cs = void $ pure p #+ map pure cs
